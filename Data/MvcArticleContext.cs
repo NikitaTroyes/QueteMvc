e2cc1d1a08d626437f7fe5a5a59325260ceb9d8a
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-11-06 21:38:54 +0100

    Add files via upload

---
+++ Data/MvcArticleContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using questCRUD.Models;
+
+
+namespace questCRUD.Data
+{
+    public class MvcArticleContext :DbContext
+    {
+        public MvcArticleContext (DbContextOptions<MvcArticleContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Articles> Article { get; set; }
+    }
+}
---
+++ Models/Articles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace questCRUD.Models
+{
+    public class Articles
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string texte { get; set; }
+    }
+}
